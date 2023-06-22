e29fb6cc240d282f422573f3640b59ea4432ad9b
khalil1444 <122092474+khalil1444@users.noreply.github.com>
2023-06-22 15:21:54 +0200

    added the function start game and the function playMatch

--- app.js
+++ app.js
@@ -3,18 +3,47 @@ const game=()=>{
     let cScore=0;
 
     //start game
-    const startGame=()=>{
-        const playBtn=document.querySelector('.intro button');
-        const introScreen=document.querySelector('.intro');
-        const match=document.querySelector('.match');
-        playBtn.addEventListener('click', ()=>{
-            introScreen.classList.add('fadeOut');
-            match.classList.add('fadeIn');
-        });
-    }
-    startGame();
+    const startGame = () => {
+        const playBtn = document.querySelector(".intro button");
+        const introScreen = document.querySelector(".intro");
+        const match = document.querySelector(".match");
     
+        playBtn.addEventListener("click", () => {
+          introScreen.classList.add("fadeOut");
+          match.classList.add("fadeIn");
+        });
+      };
+    
+    //play match
+    const playMatch = () => {
+        const options = document.querySelectorAll(".options button");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+        const hands = document.querySelectorAll(".hands img");
+        //computer choice
+        const computerOptions=["rock","paper","scissors"];
+        options.forEach(option=>{
+            option.addEventListener("click",function(){
+                const computerNumber=Math.floor(Math.random()*3);
+                const computerChoise=computerOptions[computerNumber];
+                console.log(computerChoise);
+            })
+        })
+      
+
+
+    
+    
+
+
+};    
+startGame();
+playMatch();
 }
+
+
+
 
 //call the function
 game();
+
